6c4368b43419ffff2a637aeb247c6436d0cca02c
Matthias Derer <matthias.derer@haufe-akademie.de>
2026-01-26 12:04:15 +0100

    fix harpoon keymap to not conflict with window navigation

--- lua/keymaps/harpoon.lua
+++ lua/keymaps/harpoon.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>A", function()
 	require("harpoon"):list():add()
 end)
 
-vim.keymap.set("n", "<C-h>", function()
+vim.keymap.set("n", "<leader>h", function()
 	local harpoon = require("harpoon")
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
