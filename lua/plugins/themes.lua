13a1e580ca13cda15b2b2cf34fde3a61ffd9e992
Matthias Derer <matthias.derer@haufe-akademie.de>
2026-02-02 15:56:11 +0100

    make comments italic in koda theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,5 +6,10 @@ return {
 		"oskarnurm/koda.nvim",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
+		opts = {
+			styles = {
+				comments = { italic = true },
+			},
+		},
 	},
 }
