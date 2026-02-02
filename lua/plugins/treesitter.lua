e6c54ebfbab929ccf1cbeab3ae82cd3853f040d1
Matthias Derer <matthias.derer@haufe-akademie.de>
2026-02-02 17:10:56 +0100

    fix treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,17 +3,29 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
+		config = function(_, opts)
+			local parsers = {
 				"tsx",
-				"html",
 				"typescript",
-				"javascript",
 				"lua",
-				"zig",
-				"go",
 				"bash",
-			},
-		},
+				"html",
+				"zig",
+				"javascript",
+				"go",
+			}
+
+			local Ts = require("nvim-treesitter")
+			Ts.install(parsers)
+
+			for _, p in ipairs(parsers) do
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = { p },
+					callback = function()
+						vim.treesitter.start()
+					end,
+				})
+			end
+		end,
 	},
 }
