311f320dabf7201548109801439bc1778093327b
Matthias Derer <matthias.derer@gmail.com>
2026-04-18 23:29:58 +0200

    fix(treesitter): use master branch for Neovim 0.11 compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,23 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
 		lazy = false,
 		build = ":TSUpdate",
-		config = function(_, opts)
-			local parsers = {
-				"tsx",
-				"typescript",
-				"lua",
-				"bash",
-				"html",
-				"zig",
-				"javascript",
-				"go",
-			}
-
-			local filetypeMap = {
-				tsx = "typescriptreact",
-			}
-
-			local Ts = require("nvim-treesitter")
-			Ts.install(parsers)
-
-			for _, p in ipairs(parsers) do
-				local pattern = filetypeMap[p] or p
-				vim.api.nvim_create_autocmd("FileType", {
-					pattern = { pattern },
-					callback = function()
-						vim.treesitter.start()
-					end,
-				})
-			end
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"tsx",
+					"typescript",
+					"lua",
+					"bash",
+					"html",
+					"zig",
+					"javascript",
+					"go",
+				},
+				highlight = { enable = true },
+			})
 		end,
 	},
 }
