32c6667c86a3a137bf53fe246100c9215834e291
Matthias Derer <matthias.derer@gmail.com>
2026-06-03 21:48:05 +0200

    update trail keymaps

--- lua/keymaps/trail.lua
+++ lua/keymaps/trail.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>t", "<cmd>Trail<cr>", { desc = "Trail" })
+vim.keymap.set("n", "<leader>ts", "<cmd>Trail<cr>", { desc = "Trail" })
+vim.keymap.set("n", "<leader>tx", "<cmd>TrailStop<cr>", { desc = "Trail Stop" })
+vim.keymap.set("n", "<leader>tr", "<cmd>TrailReset<cr>", { desc = "Trail Reset" })
